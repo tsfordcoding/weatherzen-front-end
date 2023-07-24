baaee38aafdc2439f1b4c0944ca5cd8b0f54f555
Timothy Ford <71365012+tsfordcoding@users.noreply.github.com>
2023-07-24 14:04:29 -0700

    completed

---
+++ src/layout/Layout.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Header from "./Header";
+import NotFound from "./NotFound";
+import Menu from "./Menu";
+import { Route, Switch } from "react-router-dom";
+import Home from "../home/Home";
+import ObservationCreate from "../observations/ObservationCreate";
+
+function Layout() {
+  return (
+    <>
+      <Header />
+      <Menu />
+      <div className="container">
+        <Switch>
+          <Route exact={true} path="/">
+            <Home />
+          </Route>
+          <Route>
+            <ObservationCreate />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </div>
+    </>
+  );
+}
+
+export default Layout;
